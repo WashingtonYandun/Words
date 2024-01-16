8b101325666d9fbb85ba0c18c7a9fc7f0035049b
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-01-15 23:17:02 -0500

    chore: Install cors

--- gateway/index.js
+++ gateway/index.js
@@ -1,7 +1,36 @@
 import express from "express";
 
+const PORT = 3000;
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
-})
+// Enable CORS
+app.use(
+    cors({
+        credentials: true,
+        origin: FRONTEND_URL || "http://localhost:8080",
+    })
+);
+
+// Error handler
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send("Something went wrong!");
+});
+
+// Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+async function init() {
+    try {
+        app.listen(PORT);
+        console.log(`<<Listening on port http://localhost:${PORT}>>`);
+        console.log(`<<Environment: ${NODE_ENV}>>`);
+    } catch (error) {
+        console.log("Error");
+        console.error(error);
+    }
+}
+
+await init().then(r => console.log(">>Init done<<"));
