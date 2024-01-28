98bdcbfed543787ec7b91247f031be0575253d64
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-01-27 21:26:04 -0500

    feat: Wordle => Start implementing wordle apis

---
+++ wordle/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import { router } from "./src/wordle.routes.js";
+
+const PORT = 3003;
+const app = express();
+
+// Enable CORS
+app.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:8080", "http://localhost:3001", "http://localhost:3002"],
+    })
+);
+
+// Error handler
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send(">> WORDLE == Something went wrong!");
+});
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.use(router);
+
+async function init() {
+    try {
+        app.listen(PORT);
+        console.log(`<< WORDLE == Listening on port http://localhost:${PORT}>>`);
+    } catch (error) {
+        console.log("Error");
+        console.error(error);
+    }
+}
+
+await init().then(r => console.log(">> WORDLE == Init done<<"));
