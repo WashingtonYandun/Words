5486f61190b15da77c11be219c104daa69b0bedc
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-01-27 20:28:03 -0500

    feat: Hangman & Words => Test route

---
+++ hangman/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import { router } from "./src/hangman.routes.js";
+
+const PORT = 3002;
+const app = express();
+
+// Enable CORS
+app.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:8080", "http://localhost:3001", "http://localhost:3003"],
+    })
+);
+
+// Error handler
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send(">> HANGMAN == Something went wrong!");
+});
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.use(router);
+
+async function init() {
+    try {
+        app.listen(PORT);
+        console.log(`<< HANGMAN == Listening on port http://localhost:${PORT}>>`);
+    } catch (error) {
+        console.log("Error");
+        console.error(error);
+    }
+}
+
+await init().then(r => console.log(">> HANGMAN == Init done<<"));
